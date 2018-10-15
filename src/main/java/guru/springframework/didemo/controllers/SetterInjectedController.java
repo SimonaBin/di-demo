dc82c9d37d76ddbcd2ca718e75b0ce1f9a2306cb
simona <simona>
2018-10-15 20:18:21 +0300

    dependency injection with SpringContext, not manually like last time

--- src/main/java/guru/springframework/didemo/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/didemo/controllers/SetterInjectedController.java
@@ -1,13 +1,19 @@
 package guru.springframework.didemo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
 import guru.springframework.didemo.services.GreetingService;
 
+@Controller
 public class SetterInjectedController {
 	public GreetingService greetingService;
 	
-	String sayHello() {
+	public String sayHello() {
 		return greetingService.sayGreeting();
 	}
+	
+	@Autowired
 	public GreetingService setGreetingService(GreetingService greetingService){
 		return this.greetingService = greetingService;
 	}
