d1372b7119ea07c780b539d054d6e29536dcb34d
simona <simona>
2018-10-17 20:59:44 +0300

    @Primary annotation use. when we have more beans from the same type and we want to specify the default one

--- src/main/java/guru/springframework/didemo/controllers/MyController.java
+++ src/main/java/guru/springframework/didemo/controllers/MyController.java
@@ -2,11 +2,19 @@ package guru.springframework.didemo.controllers;
 
 import org.springframework.stereotype.Controller;
 
+import guru.springframework.didemo.services.GreetingService;
+
 @Controller
 public class MyController {
+	
+	private GreetingService greetingService;
 
+	public MyController(GreetingService greetingService) {
+		this.greetingService = greetingService;
+	}
+	
 	public String hello() {
 		System.out.println("Hello!!!!");
-		return "foo";
+		return greetingService.sayGreeting();
 	}
 }
