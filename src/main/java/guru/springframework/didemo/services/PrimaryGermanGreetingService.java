7c61c0a4a67e440137cbaf7b09911418d363cae9
simona <simona>
2018-10-20 21:53:44 +0300

    default profile when there is no other active profile

--- src/main/java/guru/springframework/didemo/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/didemo/services/PrimaryGermanGreetingService.java
@@ -5,12 +5,13 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile({"en","default"})
 @Primary
-public class PrimaryGreetingService implements GreetingService{
+@Profile("de")
+class PrimaryGermanGreetingService implements GreetingService{
 
 	@Override
 	public String sayGreeting() {
-		return "Hello - From PrimaryGreetingService";
+		return "Hello from GermanService :)";
 	}
+
 }
